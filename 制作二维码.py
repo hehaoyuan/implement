7c7d69c8f57a6d86654c2c46cdaef323f3856f2c
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-09-16 10:12:44 +0800

    Update 制作二维码.py

--- 制作二维码.py
+++ 制作二维码.py
@@ -1,4 +1,4 @@
-
 from MyQR import myqr
-myqr.run(words='https://www.cnblogs.com/hetaoyuan/',picture='hhy.png',colorized=True)
+myqr.run(words='https://www.cnblogs.com/hetaoyuan/',picture='hhyuan.png',colorized=True,brightness=1.0,
+         contrast=1.0)
 
